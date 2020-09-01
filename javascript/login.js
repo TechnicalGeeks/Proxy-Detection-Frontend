6b36389d4c8af9cef6185a502cc1eb8dba7de603
rushabhporwal29 <45138812+rushabhporwal29@users.noreply.github.com>
2020-09-01 14:45:34 +0530

    Made some changes

---
+++ javascript/login.js
@@ -0,0 +1,14 @@
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+
+
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
