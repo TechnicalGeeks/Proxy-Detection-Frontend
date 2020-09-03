6416348f657bc45db9cbaf21f3f6e5e86029c84c
rushabhporwal29 <45138812+rushabhporwal29@users.noreply.github.com>
2020-09-03 14:25:20 +0530

    Modified Login Page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,6 +1,50 @@
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const email = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (email === "user" && password === "user") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+          firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+             // Handle Errors here.
+             var errorCode = error.code;
+             var errorMessage = error.message;
+             // ...
+           });
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+
+// const login=document.querySelector('[login]')
+
+// login.addEventListener('click',() =>{
+//   const email=document.querySelector('[username]')
+//   const password=document.querySelector('[password]')
+//   console.log("Email :",email)
+//   console.log("Password :",password)
+  
+//   /*firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+//   });*/
+// })
+
+
+
+
+// /*firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+// });*/
